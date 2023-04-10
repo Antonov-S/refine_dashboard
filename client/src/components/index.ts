c045bd82f459f62b4415e997e78ba96dadc4ecd2
Antonov-S <anto1nov1@gmail.com>
2023-04-11 00:09:22 +0300

    first commit

---
+++ client/src/components/index.ts
@@ -0,0 +1,17 @@
+// common
+import Profile from "./common/Profile";
+import PropertyCard from "./common/PropertyCard";
+import CustomButton from "./common/CustomButton";
+
+// charts
+import PieChart from "./charts/PieCart";
+import PropertyReferrals from "./charts/PropertyReferrals";
+import TotalRevenue from "./charts/TotalRevenue";
+
+// agent
+import AgentCard from "./agent/AgendCard";
+
+// home
+import TopAgent from "./home/TopAgent";
+
+export { Profile, PropertyCard, CustomButton, PieChart, PropertyReferrals, TotalRevenue, AgentCard, TopAgent };
